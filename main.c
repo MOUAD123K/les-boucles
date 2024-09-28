7260a4825491252e8d4936ed1df5e23805e2df6f
MOUAD <khshasht@gmail.com>
2024-09-28 16:04:47 +0200

    testing2

---
+++ main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+
+/*
+int main()
+
+{
+    float Longueure , larguer ;
+    float  aire , perimetre ;
+    printf("veuillez entrer  longueur : ");
+    scanf("%f",&aire);
+    printf("veuillez entrer larguer : ");
+    scanf("%f",&perimetre);
+    aire = (Longueure + larguer)*2 ;
+    perimetre = (Longueure * larguer) ;
+    printf(" aire : %.2f ",aire);
+    printf("la perimetre : %.2f ",perimetre);
+
+
+
+
+
+
+
+
+    return 0;
+}
+*/
+
+/*
+int main(){
+
+    int N ;
+    printf("saisir tan age : ");
+    scanf("%d",&N);
+    if(N >=18)
+        printf("vous etes eligible pour voter");
+    else
+        printf("vous n etes pas eligible pour voter");
+
+
+
+   return 0;
+}
+*/
+
+int main ()
+{
+    int N , i , N1 , N2 , N3 , N4 , N5 ;
+    printf("veuillez sasir le 5 entiers : ");
+    scanf("%d %d %d %d %d"&N1 , &N2,&N3,&N4,&N5);
+    N = N1 + N2 + N3 + N4 + N5;
+    if (N > 1 )
+       for(i=0;i<=5;i++){
+            N*i
+
+
+    }
+
+
+
+
+
+
+}
+
+
